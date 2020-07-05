9f486b340dbd8e3ec80d5b0d0b9c442c7fe4c1b2
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-07-05 17:42:07 +0200

    Connection back: basic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return <div>Hello world!</div>;
-}
+const App = () => {
+  const [data, setData] = useState<any>();
 
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/homepage/");
+      const testFetch = await res.json();
+      setData(testFetch);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {data?.results?.map((item: { title: any; name: any }) => (
+        <div key={item.name}>{item.name}</div>
+      ))}
+    </>
+  );
+};
 export default App;
