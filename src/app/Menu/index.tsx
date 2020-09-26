6f8e15388358252bbbaad341a4d59a3dd8432a7e
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-09-26 21:24:34 +0200

    basic menu, withRouter

--- src/app/Menu/index.tsx
+++ src/app/Menu/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <>
-      <div>Menu</div>
-    </>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Link to={""}>Menu</Link>
+      <Link to={"/shop"}>Shop</Link>
+      <div>Connect</div>
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<>Loading</>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById("app")
 );
