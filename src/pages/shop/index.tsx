b7443a8d21845bc0e18b4dc8531c739576b245f2
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-10-03 00:21:04 +0200

    add config lint

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import shopClient from "../../services/api/shop_client";
-import { Product } from "../../services/type/product";
+import React, { useState, useEffect } from 'react';
+import shopClient from '../../services/api/shop_client';
+import { Product } from '../../services/type/product';
 
 const ShoppingList = () => {
-  const [data, setData] = useState<any>();
+    const [data, setData] = useState<any>();
 
-  const fetchData = async () => {
-    setData(await shopClient.getProducts());
-  };
+    const fetchData = async () => {
+        setData(await shopClient.getProducts());
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  return (
-    <>
-      <div>shopping list</div>
-      {data?.results?.map((product: Product) => (
-        <div key={product.name}>{product.is_linked && product.name}</div>
-      ))}
-    </>
-  );
+    return (
+        <>
+            <div>shopping list</div>
+            {data?.results?.map((product: Product) => (
+                <div key={product.name}>{product.is_linked && product.name}</div>
+            ))}
+        </>
+    );
 };
 
 export default ShoppingList;
--- src/services/api/shop_client.ts
+++ src/services/api/shop_client.ts
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
-import ApiClient, { getHeaders } from "./api_client";
+import ApiClient, { getHeaders } from './api_client';
 
 class ShoppingList extends ApiClient {
-  public constructor() {
-    super();
-    this.paths = {
-      getProducts: "/products/",
-    };
-  }
-  public getProducts = async (): Promise<any> => {
-    const response = await axios.get(this.url("getProducts"), {
-      headers: getHeaders(),
-    });
+    public constructor() {
+        super();
+        this.paths = {
+            getProducts: '/products/',
+        };
+    }
+    public getProducts = async (): Promise<any> => {
+        const response = await axios.get(this.url('getProducts'), {
+            headers: getHeaders(),
+        });
 
-    return response.data;
-  };
+        return response.data;
+    };
 }
 
 const shopClient = new ShoppingList();
