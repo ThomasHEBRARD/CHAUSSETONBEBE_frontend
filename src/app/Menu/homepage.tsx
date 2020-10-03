14d8cbe768960d78b5f914ea29145825be1ff79e
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-10-03 11:54:02 +0200

    Selection sections

---
+++ src/app/Menu/homepage.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+const StyledSelection = styled.div`
+    position: relative;
+    width: 30%;
+    height: 350px;
+    background-color: white;
+    border: solid 1px black;
+`;
+
+const StyledSelectionSection = styled.div`
+    margin: 100px 5%;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const StyledSelectionButton = styled(Link)`
+    background-color: white;
+    border: solid 1px;
+    border-color: #e0e0e0;
+    border-radius: 3px;
+    text-transform: uppercase;
+    font-family: Source Sans Pro, arial, sans-serif;
+    font-weight: 700;
+    font-size: 1rem;
+    letter-spacing: 0.12rem;
+    cursor: pointer;
+    color: #3f3f3f;
+`;
+
+const StyledSelectionInfo = styled.div`
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+`;
+
+const SelectionModule = (props: { label: string; to: string }) => {
+    const { label, to } = props;
+    return (
+        <StyledSelection>
+            <StyledSelectionInfo>
+                <div>Sélection</div>
+                <div style={{ fontWeight: 'bold' }}>{label}</div>
+                <StyledSelectionButton style={{ textDecoration: 'none' }} to={to}>
+                    Découvrir
+                </StyledSelectionButton>
+            </StyledSelectionInfo>
+        </StyledSelection>
+    );
+};
+
+const HomePage = () => {
+    return (
+        <StyledSelectionSection>
+            <SelectionModule label={'Nos chaussons'} to={'/shop'} />
+            <SelectionModule label={'Nos gilets'} to={'/'} />
+            <SelectionModule label={'Nos ...'} to={'/'} />
+        </StyledSelectionSection>
+    );
+};
+
+export default HomePage;
--- src/app/Menu/index.tsx
+++ src/app/Menu/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
-  return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Link to={""}>Menu</Link>
-      <Link to={"/shop"}>Shop</Link>
-      <div>Connect</div>
-    </div>
-  );
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Link to={'/homepage'}>Menu</Link>
+            <Link to={'/shop'}>Shop</Link>
+            <div>Connect</div>
+        </div>
+    );
 };
 
 export default Menu;
