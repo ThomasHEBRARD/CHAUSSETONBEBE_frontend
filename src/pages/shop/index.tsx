1a46fc56d44e9c74d077f9c449617a199dd929ba
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-10-10 11:50:49 +0200

    quick fixes

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -16,10 +16,9 @@ const StyledProductContainer = styled.div`
 
 const ProductDisplay = (props: ProductProps) => {
     const { name, price, image, description } = props;
-    console.log(image)
     return (
         <StyledProductContainer>
-            <img src={image} alt={"chausson"} width="100" height="100"/>
+            {image && <img src={image} alt={description} width="100" height="100" />}
             <StyledProductName>{name}</StyledProductName>
             <StyledProductDescription>{description}</StyledProductDescription>
             <StyledProductPrice>{price} €</StyledProductPrice>
