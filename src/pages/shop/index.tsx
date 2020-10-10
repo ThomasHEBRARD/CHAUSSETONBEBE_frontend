8bcc7c224c3fb9bf280e408f02e08d5c3e3308e2
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-10-10 12:28:02 +0200

    quick fixes

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from 'react';
 import shopClient from '../../services/api/shop_client';
 import { ProductProps } from '../../services/type/product';
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
 const StyledProductName = styled.div``;
 const StyledProductPrice = styled.div``;
 const StyledProductDescription = styled.div``;
-const StyledProductContainer = styled.div`
+const StyledProductContainer = styled.div<{ image: boolean }>`
     display: inline-block;
     text-align: center;
-    padding: 300px 10px 10px 10px;
-    border: solid 1px red;
+    ${(props) =>
+        props.image
+            ? css`
+                  padding: 10px;
+              `
+            : css`
+                  padding: 300px 10px 10px 10px;
+              `}
+    border: solid 1px #D3D3D3;
     border-radius: 5px;
 `;
 
 const ProductDisplay = (props: ProductProps) => {
     const { name, price, image, description } = props;
     return (
-        <StyledProductContainer>
-            {image && <img src={image} alt={description} width="100" height="100" />}
+        <StyledProductContainer image={image}>
+            {image && <img src={image} alt={description} width="100%" height="300px" />}
             <StyledProductName>{name}</StyledProductName>
             <StyledProductDescription>{description}</StyledProductDescription>
             <StyledProductPrice>{price} €</StyledProductPrice>
