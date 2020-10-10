ae35d202a6dad3606791498b77cb6ea611fa1230
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-10-10 20:56:06 +0200

    Menu style

--- src/app/Footer/index.tsx
+++ src/app/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-    return <div style={{ backgroundColor: 'green' }}>Footer</div>;
+    return <div style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)' }}></div>;
 };
 
 export default Footer;
--- src/app/Menu/index.tsx
+++ src/app/Menu/index.tsx
@@ -6,19 +6,42 @@ const StyledMenu = styled.div`
     position: fixed;
     top: 0%;
     width: 100%;
-    line-height: 50px;
-    background-color: rgba(255,255,255,0.2);
+    line-height: 70px;
+    background-color: rgba(255, 255, 255, 0.2);
     display: flex;
-    justify-content: space-between;
+    padding-right: 500px;
+    justify-content: space-around;
+`;
+
+const StyledButtonMenu = styled(Link)`
+    text-decoration: none;
+    color: #3f3f3f;
+    font-weight: 400;
+    font-size: 1.3rem;
+    letter-spacing: 0.1rem;
+    text-transform: uppercase;
+    font-family: Lora, georgia, serif;
+    -webkit-font-smoothing: antialiased;
+    cursor: pointer;
+`;
+
+const StyledSeparationBar = styled.div`
+    margin-top: 80px;
+    width: 90%;
+    margin-left: 5%;
+    height: 1px;
+    background-color: grey;
 `;
 
 const Menu = () => {
     return (
-        <StyledMenu>
-            <Link to={'/homepage'}>Menu</Link>
-            <Link to={'/shop'}>Shop</Link>
-            <div>Connect</div>
-        </StyledMenu>
+        <>
+            <StyledMenu>
+                <StyledButtonMenu to={'/homepage'}>Menu</StyledButtonMenu>
+                <StyledButtonMenu to={'/shop'}>Shop</StyledButtonMenu>
+            </StyledMenu>
+            <StyledSeparationBar />
+        </>
     );
 };
 
